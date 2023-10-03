25504353ba29f2f8ec989f0c36d867d96da2c5d7
Aaron Battman <aaron.k.battman@outlook.com>
2023-10-03 11:57:23 +0100

    some basic integer and float play

--- integer.rb
+++ integer.rb
@@ -10,11 +10,49 @@ Division (/)
 
 Exponentiation (**)
   Exponentiation raises one number (the base) to the power of the other (the exponent).
+  eg: 2**2 == (((2*2)*2)*2)
+  => 16
 
 Modulo (%)
   Modulo returns the remainder of division.
 
 =end
+
+#####
+
+a = 12
+
+a.class
+# => Integer
+
+a.to_f # to float - decimalises
+# => 12.0
+
+a.class # has not modified variable
+# => Integer
+
+a
+# => 12
+
+b = a.to_f
+# => 12.0
+
+c = a / 4.5 # including a Float will return a float 
+# => 2.6666666666666665
+
+c.class
+# => Float
+
+c.to_i # TO REVISE - to_i rounding conditions??
+# => 2
+
+c.round # mathmatically rounds and converts to Integer
+# => 3
+
+c.truncate # rounds down/towards 0 and converts to Integer
+# => 2
+
+#####
 
 num_arr_x = (1..10).to_a
 # => [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
@@ -26,8 +64,8 @@ num_arr_z = [*21..30]
 # => [21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
 
 
-num_arr_y.class
+num_arr_x.class
 # => Array
 
-num_arr_z[4].class
+num_arr_y[4].class
 # => Integer
